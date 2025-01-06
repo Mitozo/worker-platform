e40616d2f1961b5043d1f5044cff1e7e7cbdd095
Tsilavina Randrian <tsilavinandriana@gmail.com>
2025-01-06 15:48:06 +0300

    remove custom type to be tested in a different branch

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -9,17 +9,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    // protected function initializeContainer(): void
-    // {
-    //     parent::initializeContainer();
-
-    //     $container = $this->getContainer();
-        
-    //     // Register EnumTypes before Doctrine tries to use them
-    //     if ($container->has(EnumTypeFactory::class)) {
-    //         $enumFactory = $container->get(EnumTypeFactory::class);
-    //         $enumFactory->registerEnumTypes();  // Ensure enum types are registered
-    //     }
-    // }
 }
