19f0bec3acdb37bead60c18b3359109aad4ef365
Tsilavina Randrian <tsilavinandriana@gmail.com>
2025-01-08 16:35:53 +0300

    feat #001 change updatefieldtrait to consider null value

---
+++ app/migrations/Version20250108091956.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250108091956 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE file CHANGE updated_at updated_at DATETIME on update CURRENT_TIMESTAMP, CHANGE updated_by updated_by VARCHAR(20) DEFAULT NULL');
+        $this->addSql('ALTER TABLE file_class CHANGE type type ENUM(\'PRODUCTION\', \'CORRECTION\', \'TEST\', \'FORMATION\')');
+        $this->addSql('ALTER TABLE file_data CHANGE updated_at updated_at DATETIME on update CURRENT_TIMESTAMP, CHANGE updated_by updated_by VARCHAR(20) DEFAULT NULL');
+        $this->addSql('ALTER TABLE file_details CHANGE updated_at updated_at DATETIME on update CURRENT_TIMESTAMP, CHANGE updated_by updated_by VARCHAR(20) DEFAULT NULL');
+        $this->addSql('ALTER TABLE treatment CHANGE updated_at updated_at DATETIME on update CURRENT_TIMESTAMP, CHANGE updated_by updated_by VARCHAR(20) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE treatment CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE updated_by updated_by VARCHAR(20) NOT NULL');
+        $this->addSql('ALTER TABLE file CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE updated_by updated_by VARCHAR(20) NOT NULL');
+        $this->addSql('ALTER TABLE file_details CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE updated_by updated_by VARCHAR(20) NOT NULL');
+        $this->addSql('ALTER TABLE file_class CHANGE type type VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE file_data CHANGE updated_at updated_at DATETIME DEFAULT NULL, CHANGE updated_by updated_by VARCHAR(20) NOT NULL');
+    }
+}
